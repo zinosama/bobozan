e836959b67057c229de7b234ce7b6759c8a1f561
zino <rhu5@u.rochester.edu>
2018-12-15 23:56:49 -0500

    working commit

---
+++ index.js
@@ -0,0 +1,144 @@
+var myId, yourId, conn;
+var peer = new Peer();
+var myAction, yourAction;
+var zanCnt = 0;
+const actions = ['zan', 'defense', 'sbo', 'mbo', 'lbo']
+const actionMap = {
+  zan: '攒',
+  defense: '防',
+  sbo: '小波',
+  mbo: '中波',
+  lbo: '大波',
+}
+const actionScore = {
+  zan: -2,
+  defense: -1,
+  sbo: 1,
+  mbo: 2,
+  lbo: 3,
+}
+
+peer.on('open', function(id) {
+  $("#myId").text(`My ID: ${id}`);
+});
+
+peer.on('connection', function(thisCon){
+  init_connection(thisCon);
+});
+
+jQuery(function($) {
+  $("#connectBtn").click(function(el){
+    const peerId = $('#connection-id')[0].value;
+    init_connection(peer.connect(peerId));
+  });
+
+  actions.forEach(function(action){
+    $(`#${action}`).click(function(el){
+      const selectedAction = el.target.id;
+      if (validate_action(selectedAction)) {
+        set_my_action(selectedAction)
+        conn.send(selectedAction);
+        update_status();
+      } else {
+        window.alert('invalid action');
+      }
+    })
+  })
+
+});
+
+function init_connection(thisCon) {
+  conn = thisCon;
+  $('#connectForm').remove();
+  $('#p1').text("Me: ");
+  $('#p2').text(`${conn.peer}: `);
+  attach_receive_handler();
+  console.log("connection received");
+}
+
+function validate_action(action){
+  if (action === 'sbo' && zanCnt < 1) {
+    return false
+  } else if (action == 'mbo' && zanCnt < 2) {
+    return false
+  } else if (action == 'lbo' && zanCnt < 3) {
+    return false
+  }
+  return true
+}
+
+function attach_receive_handler() {
+  conn.on('data', function(data) {
+    set_your_action(data);
+    console.log('Received', data);
+    update_status();
+  });
+}
+
+function update_status() {
+  if (myAction && yourAction) {
+    console.log(`${myAction}, ${yourAction}`);
+    if (myAction === 'zan') {
+      update_zancnt(1);
+    } else if (myAction === 'sbo') {
+      update_zancnt(-1);
+    } else if (myAction === 'mbo') {
+      update_zancnt(-2);
+    } else if (myAction === 'lbo') {
+      update_zancnt(-3);
+    }
+
+    const myScore = actionScore[myAction];
+    const yourScore = actionScore[yourAction];
+    if (
+      (myScore < 0 && yourScore < 0) ||
+      (myScore === yourScore) ||
+      (myScore + yourScore === 0)
+    ) {
+      clear_actions();
+    } else {
+      debugger
+      myScore > yourScore ? i_win() : i_lose();
+    }
+  }
+}
+
+function set_my_action(action){
+  myAction = action;
+  console.log("my new action", action);
+  const description = action ? actionMap[myAction] : ''
+  $('#p1Action').text(description);
+}
+
+function set_your_action(action){
+  yourAction = action;
+  console.log("your new action", action);
+  const description = action ? actionMap[yourAction] : ''
+  $('#p2Action').text(description);
+}
+
+function update_zancnt(change){
+  if (change) {
+    zanCnt += change;
+  } else {
+    zanCnt = 0;
+  }
+  $('#zanCnt').text(zanCnt);
+}
+
+function clear_actions() {
+  set_my_action(null);
+  set_your_action(null);
+}
+
+function i_win(){
+  window.alert('You win!');
+  clear_actions();
+  update_zancnt(null);
+}
+
+function i_lose() {
+  window.alert('You lost!');
+  clear_actions();
+  update_zancnt(null);
+}
